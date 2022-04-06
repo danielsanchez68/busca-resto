16f6a2b8ae02cae927c545f25cbbe4ee624d029d
danielsanchez68 <danielsanchez68@hotmail.com>
2022-04-06 09:03:06 -0300

    v8

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const STR_CNX = process.env.STR_CNX || 'mongodb://localhost:27017/mibase'
+const VERSION = 'V8'
 
 module.exports = {
-    STR_CNX
+    STR_CNX,
+    VERSION
 }
